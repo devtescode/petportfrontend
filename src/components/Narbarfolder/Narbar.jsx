a0e2f2101efb51a617ccafd93fed5aa1f6a88d7e
TESCODE <teslimagboola09@gmail.com>
2024-06-06 10:22:28 +0100

    frontend

--- src/components/Narbarfolder/Narbar.jsx
+++ src/components/Narbarfolder/Narbar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { useNavigate } from 'react-router'
 
 const Narbar = () => {
-    const navigate = useNavigate()
-    const contactnavigate =()=>{
-        navigate('/contact')
-    }
-    const Homeroute = () => {
-        navigate('/')
-    }
-    
+  const navigate = useNavigate()
+  const contactnavigate = () => {
+    navigate('/contact')
+  }
+  const Homeroute = () => {
+    navigate('/')
+  }
+
   return (
     <>
-     {/* <div className="container-fluid d-none d-lg-block bg-dark text-white" style={{cursor: "pointer", position:"fixed", top:"0", zIndex:"100"}}>
+      {/* <div className="container-fluid d-none d-lg-block bg-dark text-white" style={{cursor: "pointer", position:"fixed", top:"0", zIndex:"100"}}>
         <div className="row gx-0">
           <div className="col-lg-4 text-center py-2">
             <div className="d-inline-flex align-items-center">
@@ -44,7 +44,7 @@ const Narbar = () => {
         </div>
       </div> */}
       <nav className="fixed-top navbar navbar-expand-lg bg-white navbar-light p-2">
-        <a onClick={Homeroute} style={{cursor: "pointer"}}  className="navbar-brand ms-lg-5">
+        <a onClick={Homeroute} style={{ cursor: "pointer" }} className="navbar-brand ms-lg-5">
           <h1 className="m-0 text-uppercase text-dark">
             <i className="bi bi-shop fs-1 text-primary me-3" />
             Petport
@@ -58,7 +58,7 @@ const Narbar = () => {
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse" style={{cursor: "pointer"}}>
+        <div className="collapse navbar-collapse" id="navbarCollapse" style={{ cursor: "pointer" }}>
           <div className="navbar-nav ms-auto text-center">
             <a onClick={Homeroute} className="nav-item nav-link text-dark">
               Home
@@ -66,15 +66,22 @@ const Narbar = () => {
             <a className="nav-item nav-link text-dark">
               About
             </a>
-            <a  className="nav-item nav-link text-dark">
+            <a className="nav-item nav-link text-dark">
               Service
             </a>
-            <a  className="nav-item nav-link text-dark">
+            <a className="nav-item nav-link text-dark">
               Product
             </a>
+            <a className="nav-item nav-link text-dark fw-bold ">
+              Register
+            </a>
+            <a className="nav-item nav-link text-dark fw-bold">
+              Login
+            </a>
+
             <a
-              
-              className="nav-item nav-link nav-contact text-white px-5 ms-lg-5 contant-topstyle"
+
+              className="nav-item nav-link nav-contact text-white px-3 ms-lg-5 bg-dark rounded-4 w-50 mx-auto"
               onClick={contactnavigate}
             >
               Contact <i className="bi bi-arrow-right" />
