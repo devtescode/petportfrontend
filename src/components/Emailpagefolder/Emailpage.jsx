bdcb3e84dc421192b1fe6329fda54b7c9c943dee
TESCODE <teslimagboola09@gmail.com>
2024-06-16 00:57:31 +0100

    frontend Emailpage

--- src/components/Emailpagefolder/Emailpage.jsx
+++ src/components/Emailpagefolder/Emailpage.jsx
@@ -1,98 +1,77 @@
-import React, { useState } from 'react'
-import { useFormik } from 'formik'
-import axios from 'axios'
-import * as Yup from "yup"
-import { Navigate, useNavigate } from 'react-router'
-import Loader from '../Loaderfolder/Loaderpage'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import axios from 'axios';
+import * as Yup from "yup";
+import { useNavigate } from 'react-router';
+import Loader from '../Loaderfolder/Loaderpage';
 
 const Emailpage = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
-    function generateRandomToken() {
-        const min = 1000;
-        const max = 9999;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    const randomToken = generateRandomToken();
-    console.log(randomToken);
 
     const formik = useFormik({
         initialValues: {
             emailpage: ""
         },
         validationSchema: Yup.object({
-            emailpage: Yup.string()
-                .required('Required'),
+            emailpage: Yup.string().required('Required'),
         }),
         onSubmit: values => {
-          
             setLoading(true);
-            let successMessage, errorMessage;
-            axios.post("http://localhost:5000/useranimalinvest/emailpage", { Emailpage: values.emailpage, randomToken }).then((response) => {
-                successMessage = response.data.message;
-                errorMessage = response.data.message;
-             
-
-                setTimeout(() => {
-                    Swal.fire({
-                        title: "",
-                        text: successMessage,
-                        icon: response.data.status ? "success" : "warning",
-                        button: response.data.status ? "Okay" : "Aww yiss!",
-                    });
-                    if (response.data.status == true) {  
-                        navigate("/forgetpassword")
-                    }
-                }, 6000);
-
-            })
-            .catch((err) => {
-                console.log(err);
-                errorMessage = err.response ? err.response.data.message : "An error occurred";
-                setTimeout(() => {
-                    Swal.fire({
-                        title: "",
-                        text: errorMessage,
-                        icon: "error",
-                        button: "Aww yiss!",
-                    });
-                }, 6000);
-            })
-            setTimeout(() => {
+            axios.post("http://localhost:5000/useranimalinvest/emailpage", { Emailpage: values.emailpage}).then((response) => {
+                const { status, message } = response.data;
                 setLoading(false);
-            }, 6000);
+                Swal.fire({
+                    title: "",
+                    text: message,
+                    icon: status ? "success" : "warning",
+                    button: status ? "Okay" : "Aww yiss!",
+                });
+                if (status) {
+                    navigate("/forgetpassword");
+                }
+            }).catch((err) => {
+                console.error(err);
+                setLoading(false);
+                Swal.fire({
+                    title: "",
+                    text: err.response ? err.response.data.message : "An error occurred",
+                    icon: "error",
+                    button: "Aww yiss!",
+                });
+            });
         }
-    })
+    });
+
     return (
         <>
-        {loading && <Loader/>}
-         <form onSubmit={formik.handleSubmit}>
-            <div className='coverupemail'>
-                <div className='mymaildiv'>
-                    <div className='mymaileachdiv'></div>
-                    <div className='mymaileachdiv2 shadow-lg'>
-                        <div class="mailform">
-                            <div>
-                                <p>Enter your email address in the input provided</p>
-                            </div>
-                            <div>
-                                <input class="mailinput mt-3" onChange={formik.handleChange} name='emailpage' value={formik.values.emailpage} placeholder="Email address" required="" type="text" />
-                                <span class="input-border1">
-                                </span>
-                            </div>
-                            <div className='text-danger text-end' style={{ position: "absolute", right: "0px" }}>
-                                {formik.errors.emailpage}
-                            </div>
-                            <div className='mt-5 MailBtnStyle'>
-                                <button className='btn btn-dark MailBtnStyle2'>Submit</button>
+            {loading && <Loader />}
+            <form onSubmit={formik.handleSubmit}>
+                <div className='coverupemail'>
+                    <div className='mymaildiv'>
+                        <div className='mymaileachdiv'></div>
+                        <div className='mymaileachdiv2 shadow-lg'>
+                            <div className="mailform">
+                                <div>
+                                    <p>Enter your email address in the input provided</p>
+                                </div>
+                                <div>
+                                    <input className="mailinput mt-3" onChange={formik.handleChange} name='emailpage' value={formik.values.emailpage} placeholder="Email address" required="" type="text" />
+                                    <span className="input-border1"></span>
+                                </div>
+                                <div className='text-danger text-end' style={{ position: "absolute", right: "0px" }}>
+                                    {formik.errors.emailpage}
+                                </div>
+                                <div className='mt-5 MailBtnStyle'>
+                                    <button className='btn btn-dark MailBtnStyle2'>Submit</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </form>
         </>
-    )
-}
+    );
+};
 
-export default Emailpage
+export default Emailpage;
--- src/components/Forgetpasswordfolder/Forgetpassword.jsx
+++ src/components/Forgetpasswordfolder/Forgetpassword.jsx
@@ -3,32 +3,84 @@ import * as Yup from "yup"
 import { useFormik } from 'formik'
 import axios from 'axios'
 import { useNavigate } from 'react-router'
-import Loader from '../Loaderfolder/Loaderpage'
+import Loaderpage from '../Loaderfolder/Loaderpage'
 const Forgetpassword = () => {
+    const navigate = useNavigate()
     const [loading, setLoading] = useState(false);
-    
+    const formik = useFormik({
+        initialValues: {
+            forgetemailpage1: "",
+            forgetemailpage2: "",
+            forgetemailpage3: "",
+        },
+        validationSchema: Yup.object({
+            forgetemailpage1: Yup.string()
+                .required('Required'),
+
+        }),
+        onSubmit: values => {
+            setLoading(true);
+            let successMessage, errorMessage;
+            axios.post("http://localhost:5000/useranimalinvest/Forgetpasspage", { Forgetmailone: values.forgetemailpage1, Forgetmailtwo: values.forgetemailpage2, Forgetmailthree: values.forgetemailpage3 })
+                .then((response) => {
+                    successMessage = response.data.message;
+                    errorMessage = response.data.message;
+                    setTimeout(() => {
+                        swal({
+                            title: "",
+                            text: successMessage,
+                            icon: response.data.status ? "success" : "warning",
+                            button: response.data.status ? "Okay" : "Aww yiss!",
+                        });
+
+                        if (response.data.status == true) {
+                            navigate("/login")
+                        }
+                    }, 6000);
+
+                })
+                .catch((err) => {
+                    console.log(err);
+                    errorMessage = err.response ? err.response.data.message : "An error occurred";
+
+                    setTimeout(() => {
+                        swal({
+                            title: "",
+                            text: errorMessage,
+                            icon: "error",
+                            button: "Aww yiss!",
+                        });
+                    }, 6000);
+                })
+            setTimeout(() => {
+                setLoading(false);
+            }, 6000);
+        }
+    })
     return (
-     // {loading && <Loader/>}
-   <form onSubmit={formik.handleSubmit}>
-       <div className='mycondiv'>
-           <div className='myeachdiv'></div>
-           <div className='myeachdiv2 shadow-lg'>
-               <div className='text-center mt-3 taketop'>
-                   <h3>Email Verification</h3>
-                   <p>We have sent a code to your email</p>
-               </div>
-               <div className='input-container mx-auto forgetinputwidthdiv mt-4'>
-                   <input className='forgetinputwidth' onChange={formik.handleChange} name='forgetemailpage1' value={formik.values.forgetemailpage1} maxLength={4} type="text" placeholder='OTP' />
-                   <input className='forgetinputwidth' onChange={formik.handleChange} name='forgetemailpage2' value={formik.values.forgetemailpage2} type="text" placeholder='Password' />
-                   <input className='forgetinputwidth' onChange={formik.handleChange} name='forgetemailpage3' value={formik.values.forgetemailpage3} type="text" placeholder='Confirm Password' />
-               </div>
-               <div className='text-center mt-3'>
-                   <button className='btn btn-dark w-75 p-2 styleBtn'>Verify Account</button>
-               </div>
-           </div>
-       </div>
-   </form>
-  )
+        <form onSubmit={formik.handleSubmit}>
+            <div className='Forgetpassdiv'>
+                {loading && <Loaderpage />}
+                <div className='mycondiv'>
+                    <div className='myeachdiv'></div>
+                    <div className='myeachdiv2 shadow-lg'>
+                        <div className='text-center mt-3 taketop'>
+                            <h3>Email Verification</h3>
+                            <p>We have sent a code to your email</p>
+                        </div>
+                        <div className='input-container mx-auto forgetinputwidthdiv mt-4'>
+                            <input className='forgetinputwidth form-control my-2 mx-auto' onChange={formik.handleChange} name='forgetemailpage1' value={formik.values.forgetemailpage1} maxLength={4} type="text" placeholder='OTP' />
+                            <input className='forgetinputwidth form-control my-2 mx-auto' onChange={formik.handleChange} name='forgetemailpage2' value={formik.values.forgetemailpage2} type="text" placeholder='Password' />
+                            <input className='forgetinputwidth form-control my-2 mx-auto' onChange={formik.handleChange} name='forgetemailpage3' value={formik.values.forgetemailpage3} type="text" placeholder='Confirm Password' />
+                        </div>
+                        <div className='text-center'>
+                            <button className='btn btn-dark styleBtn'>Verify Account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
 }
 
 export default Forgetpassword
