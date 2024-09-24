659c339529dc80bccbece02cb9cf95665f8f506f
TESCODE <teslimagboola09@gmail.com>
2024-09-24 15:29:31 +0100

    addup routing

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,5 +1,6 @@
-const baseURL = process.env.REACT_APP_API_BASE_URL;
-
+// const baseURL = process.env.REACT_APP_API_BASE_URL;
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000'; 
+console.log('Base URL:', baseURL)
 export const API_URLS = {
     signin: `${baseURL}/useranimalinvest/signin`,
     // Add more endpoints here as needed
