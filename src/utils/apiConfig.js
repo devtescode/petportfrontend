9660763ed5c11380dd897f31d5acb0eb56662b7f
TESCODE <teslimagboola09@gmail.com>
2024-09-24 15:37:47 +0100

    addup routing

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,5 +1,5 @@
-// const baseURL = process.env.REACT_APP_API_BASE_URL;
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://petportbackend.onrender.com'; 
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+// const baseURL = process.env.REACT_APP_API_BASE_URL || 'https://petportbackend.onrender.com'; 
 console.log('Base URL:', baseURL)
 export const API_URLS = {
     signin: `${baseURL}/useranimalinvest/signin`,
